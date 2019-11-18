6271071a9d38d17485304e8e73cbbc0198d55811
Kyle Jennings <Kyle22@gmail.com>
2019-11-18 13:31:13 -0700

    initial commit with model and model controller

---
+++ Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/Model Controller/PostController.swift
@@ -0,0 +1,9 @@
+//
+//  PostController.swift
+//  Post
+//
+//  Created by Kyle Jennings on 11/18/19.
+//  Copyright © 2019 DevMtnStudent. All rights reserved.
+//
+
+import Foundation
---
+++ Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/Model/Post.swift
@@ -0,0 +1,21 @@
+//
+//  Post.swift
+//  Post
+//
+//  Created by Kyle Jennings on 11/18/19.
+//  Copyright © 2019 DevMtnStudent. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    var text: String
+    var username: String
+    var timestamp: TimeInterval
+    
+    init(text: String, username: String, timestamp: TimeInterval = Date().timeIntervalSince1970) {
+        self.text = text
+        self.username = username
+        self.timestamp = timestamp
+    }
+}
